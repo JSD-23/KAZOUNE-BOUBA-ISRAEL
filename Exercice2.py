881f2e2be3af25626b5b31b0d163cc1bb28beaed
Kabois <kazouneisraelbouba@gmail.com>
2023-11-03 10:35:42 +0100

    Kazoune JSD

--- Exercice2.py
+++ Exercice2.py
@@ -1,4 +1,13 @@
-def maxProfit(prix):
-  profits = [profi.split(",") for profi in prix]
-  return 0
-#maxProfit('7,1,5,3,6,4')
+def maxprofit(prix):
+  mes_achats = prix[0]
+  profits = 0
+  #contrôle si on  peut/non  realiser le profit
+  if not prix:
+    return 0
+  for i in range(1, len(prix)):
+    if prix[i] < mes_achats:
+      mes_achats = prix[i]
+    else:
+      profit = max(profits, prix[i] - mes_achats)
+    return profits
+#maxprofit([7,1,5,3,6,4])
